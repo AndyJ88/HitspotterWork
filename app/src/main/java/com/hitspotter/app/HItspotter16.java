33db4d382974d7d68ae072c568c914db67273e85
AndyJ88 <jagandy602@gmail.com>
2021-08-17 18:28:23 +0200

    Hitspotter bundle

---
+++ app/src/main/java/com/hitspotter/app/HItspotter16.java
@@ -0,0 +1,14 @@
+package com.hitspotter.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HItspotter16 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hitspotter16);
+    }
+}
